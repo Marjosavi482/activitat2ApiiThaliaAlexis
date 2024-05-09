95b3c200635e5801df13111d207aa4393170e06f
Tu nombre <tu@email.com>
2024-05-10 00:15:42 +0200

    falta hacer que se logee bien el usuario y controlar lo de los niveles

--- backend/usuario/login.proc.php
+++ backend/usuario/login.proc.php
@@ -1,36 +1,38 @@
 <?php
 
-    $url ="/../../api/USUARI.php";
+    $url ="../../api/USUARI.php";
+    $usu_name = $_REQUEST['usu_mail'];
+    $usu_contra = $_REQUEST['usu_contra'];
 
     $data = array(
-        'usu_mail' => $_REQUEST['usu_mail'],
-        'usu_contra' => $_REQUEST['usu_contra']
+        'usu_name' => $usu_name,
+        'usu_contra' => $usu_contra
     );
-
-    echo json_encode($data);
 
     $ch = curl_init($url);
 
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST"); // Cambiar a POST
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     $response = curl_exec($ch);
 
     if (curl_errno($ch)) {
-        echo 'Error: ' . curl_error($ch);
+        echo 'Error al realizar la solicitud: ' . curl_error($ch);
     } else {
         $json_response = json_decode($response, true);
 
-        if (isset($json_response['token'])) {
+        if ($json_response && is_array($json_response)) {
             session_start();
             $_SESSION['token'] = $json_response['token'];
-            header('Location: ../index.php');
+            header('location: index.php');
         } else {
-            header('Location: ../login.php?error=1');
+            header('location: index.php');
         }
+
     }
 
+    curl_close($ch);
 ?>
